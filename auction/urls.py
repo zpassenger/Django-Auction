92391de3874c8facd9309834ad46bd6b64165ebe
Joe curlee <joe.curlee@gmail.com>
2013-01-05 12:32:10 -0600

    changed lot detail view url structure

--- auction/urls.py
+++ auction/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
     url(r'^bids/$', auction.views.BidListView.as_view(), name='bids'),
     url(r'^bids/delete/(?P<bid_id>\d+)/$', auction.views.BidDetailView.as_view(action="delete"), name='delete_bid'),
     url(r'^(?P<slug>[0-9A-Za-z-_.]+)/$', auction.views.AuctionView.as_view(), name='auction'),
-    url(r'^(?P<auction_slug>[0-9A-Za-z-_.]+)/lot/(?P<slug>[0-9A-Za-z-_.//]+)/$', auction.views.LotDetailView.as_view(), name='lot'),
+    url(r'^(?P<auction_slug>[0-9A-Za-z-_.]+)/lot/(?P<slug>[0-9A-Za-z-_.//]+)/(?P<pk>\d+)/$', auction.views.LotDetailView.as_view(), name='lot'),
 )
