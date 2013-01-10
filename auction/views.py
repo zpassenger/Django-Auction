041be5d271f6b9ed9d05435ef33ff2fc1579654e
Joe curlee <joe.curlee@gmail.com>
2013-01-10 14:41:24 -0600

    fixed auction list view for generic fk on lot model

--- auction/views.py
+++ auction/views.py
@@ -27,7 +27,9 @@ class AuctionView(DetailView):
     
     def get_context_data(self, **kwargs):
         context = super(AuctionView, self).get_context_data(**kwargs)
-        context['object_list'] = self.object.lots.select_related('auction').all() 
+        object_type = ContentType.objects.get_for_model(self.object)
+        context['object_list'] = auctions.models.Lot.objects.filter(content_type__pk=object_type.pk,
+                                                                    object_id=self.object.pk).all()
         return context
 
 class LotDetailView(SingleObjectMixin, FormView):
