116502db4dca8fb95641502b9e4c9d35ad0dd1f5
Joe curlee <joe.curlee@gmail.com>
2013-01-11 13:19:05 -0600

    changed structure of auctions urls

--- auction/urls.py
+++ auction/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     url(r'^$', auction.views.AuctionListView.as_view(), name='auctions'),
     url(r'^bids/$', auction.views.BidListView.as_view(), name='bids'),
     url(r'^bids/delete/(?P<bid_id>\d+)/$', auction.views.BidDetailView.as_view(action="delete"), name='delete_bid'),
-    url(r'^(?P<slug>[0-9A-Za-z-_.]+)/$', auction.views.AuctionView.as_view(), name='auction'),
-    url(r'^(?P<auction_slug>[0-9A-Za-z-_.]+)/lot/(?P<slug>[0-9A-Za-z-_.//]+)/(?P<pk>\d+)/$', auction.views.LotDetailView.as_view(), name='lot'),
+    url(r'^auction/(?P<slug>[0-9A-Za-z-_.]+)/$', auction.views.AuctionView.as_view(), name='auction'),
+    url(r'^auction/(?P<auction_slug>[0-9A-Za-z-_.]+)/lot/(?P<slug>[0-9A-Za-z-_.//]+)/(?P<pk>\d+)/$', auction.views.LotDetailView.as_view(), name='lot'),
 )
