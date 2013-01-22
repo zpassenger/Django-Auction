5203994fb4311009297314d3d1bc75f5f2302097
Joe curlee <joe.curlee@gmail.com>
2013-01-22 16:33:30 -0600

    added get_lot to BidForm for simplified extending

--- auction/forms.py
+++ auction/forms.py
@@ -10,7 +10,13 @@ class BidForm(forms.Form):
         lot_id = self.data.get('lot_id')
         amount = self.data.get('amount')
         
-        lot = auction.models.Lot.objects.get(pk=lot_id)
+        lot = self.get_lot(lot_id)
 
         bidbasket = auction.utils.get_or_create_bidbasket(request)
         return bidbasket.add_bid(lot, amount)
+    
+    def get_lot(self, lot_id):
+        """
+        For simplified extending.
+        """
+        return auction.models.Lot.objects.get(pk=lot_id)
